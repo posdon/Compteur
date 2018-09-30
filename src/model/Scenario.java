5e13417142ba6699d4c635e7c9c86d0096fcb403
posdon <alexandre.gimenez-puig@insa-rennes.fr>
2018-09-30 14:29:15 +0200

    Revert "Modelize Manager of Scenario"
    This reverts commit 39238df96b7661ba2ffe6128f32d11af44a56888.

--- src/model/Scenario.java
+++ src/model/Scenario.java
@@ -15,22 +15,5 @@ public class Scenario {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Scenario other = (Scenario) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
-		
+	
 }
