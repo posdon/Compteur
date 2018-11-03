73ff7f3c231394c8c4198821e7609d2e631cbb64
posdon <alexandre.gimenez-puig@insa-rennes.fr>
2018-11-03 18:25:56 +0100

    Assure bot connection

---
+++ src/bot/CompteurBotDiscord.java
@@ -0,0 +1,59 @@
+package bot;
+
+
+
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+
+public class CompteurBotDiscord implements Runnable {
+
+	private JDA jda;
+	private String token;
+	private CompteurLogger LOG = CompteurLogger.getLogger(this.getClass().getName());
+	private boolean running;
+	private Scanner scanner = new Scanner(System.in);
+	
+	
+	public CompteurBotDiscord(String token) {
+		this.token = token;
+		
+	}
+	
+	@Override
+	public void run() {
+		try {
+			start();
+		} catch (LoginException e) {
+			running = false;
+			LOG.error("An error occured while logging.");
+			e.printStackTrace();
+		}
+		while(running) {
+			if(scanner.hasNextLine() && "quit".equals(scanner.nextLine())) setRunning(false);
+		}
+		stop();
+	}
+	
+	public void start() throws LoginException {
+		LOG.info("Bot Compteur starting...");
+		running = true;
+		jda = new JDABuilder(AccountType.BOT).setToken(token).build();
+		LOG.info("Bot Compteur started with the token '"+token+"'");
+	}
+	
+	public void stop() {
+		LOG.info("Bot Compteur will stop soon...");
+		
+		LOG.info("Bot Compteur shut down.");
+	}
+	
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+}
---
+++ src/bot/MainBot.java
@@ -0,0 +1,26 @@
+package bot;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class MainBot {
+	
+	private static Properties conf;
+	
+	private static void loadConfiguration() throws IOException {
+		conf = new Properties();
+		InputStream input = new FileInputStream("src/properties/conf.properties");
+		conf.load(input);
+	}
+
+	public static void main(String[] args) throws IOException {
+		loadConfiguration();
+		
+		CompteurBotDiscord compteurBot = new CompteurBotDiscord(conf.getProperty("token"));
+		new Thread(compteurBot, "botDiscordCompteur").start();
+	}
+	
+	
+}
